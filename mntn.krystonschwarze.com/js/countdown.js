de1e32463cd31c17626c3f4e6edbd17350fc6155
Evan Vinciguerra <evanv511@gmail.com>
2022-06-27 11:35:21 -0400

    santa lift off countdown update

--- mntn.krystonschwarze.com/js/countdown.js
+++ mntn.krystonschwarze.com/js/countdown.js
@@ -1,19 +1,30 @@
-// Set the date we're counting down to
-var countDownDate = new Date('Jan 5, 2024 15:37:25').getTime();
+// Get Santa Lift off Date
+let santaLiftOff = new Date('Dec 24, 1992 21:00:00');
+
+// Get todays date
+let now = new Date();
+let currentYear = now.getFullYear();
+
+// Set santaLiftOff year
+santaLiftOff.setFullYear(currentYear);
+
+if (now > santaLiftOff) {
+  santaLiftOff.setFullYear(currentYear + 1);
+}
 
 // Update the count down every 1 second
-var x = setInterval(function () {
+let x = setInterval(function () {
   // Get today's date and time
-  var now = new Date().getTime();
+  let now = new Date().getTime();
 
   // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+  let distance = santaLiftOff - now;
 
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
   document.getElementById('countdown').innerHTML =
